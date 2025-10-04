22e63bc2c8e01a1b2c1178b6b4ec2e4390232a9d
Nidal <nidalthegreat69664@gmail.com>
2025-10-04 15:55:57 +0530

    Home page pattern implemented , responsiveness is very bad

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
+import "./Home.css"
 
 const Home = () => {
   return (
-    <div>
-        <h1>Home</h1>
-        </div>
+     <div className="home">
+      <div className="content">
+      <img src="iste-logo.png" alt="ISTE Logo" className="logo" />
+      <h1><b>ISTE GECK</b></h1>
+      <h3>INDIAN SOCIETY FOR TECHNICAL EDUCATION</h3>
+      <p>GEC KOZHIKODE STUDENTS CHAPTER KE-70</p>
+      </div>
+    </div>
   )
 }
 
