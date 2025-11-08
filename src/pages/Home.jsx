a91f4275c59844c6cf0eabf6957f8e95cb8f0422
Nidal <nidalthegreat69664@gmail.com>
2025-11-08 13:45:22 +0530

    welcome

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,13 +4,28 @@ import "./Home.css"
 const Home = () => {
   return (
      <div className="home">
+
+<div className='wall'>
       <div className="content">
       <img src="iste-logo.png" alt="ISTE Logo" className="logo" />
       <h1><b>ISTE GECK</b></h1>
       <h3>INDIAN SOCIETY FOR TECHNICAL EDUCATION</h3>
       <p>GEC KOZHIKODE STUDENTS CHAPTER KE-70</p>
       </div>
+</div>
+     <div className="welcome-section">
+        <div className="line line-top"></div>
+        <h1 className="welcome-text">WELCOME</h1>
+        <div className="bottom-container">
+            <div className="line line-bottom"></div>
+            <span className="subtitle">GATEWAY TO EXPLORE US</span>
+        </div>
+      </div>
+
+
+
     </div>
+   
   )
 }
 
