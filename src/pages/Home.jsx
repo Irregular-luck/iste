d9089ce70347980612ca15ed5cfc9fcb970e67ca
Nidal <nidalthegreat69664@gmail.com>
2025-11-30 02:54:00 +0530

    about us

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import "./Home.css"
+import "./Home-about.css"
 
 const Home = () => {
   return (
@@ -22,6 +23,30 @@ const Home = () => {
         </div>
       </div>
 
+<div class="about-section-container">
+  <div class="about-title">ABOUT US</div>
+
+  <div class="about-content">
+    <p>The ISTE Student Chapter at Government Engineering College, Kozhikode (GECK), 
+      is one of the most active and influential student chapters under the Kerala Section of ISTE. Dedicated to fostering the holistic development of students, 
+      this chapter organizes a variety of technical, managerial, and extracurricular activities, providing a platform for members to enhance their skills and knowledge.</p>
+
+
+    <p>
+        Our chapter has earned notable recognition over the years, including the Best Student Chapter Award, 
+        awarded for our consistent excellence in conducting events, workshops, and fostering a vibrant academic community. Additionally, 
+        the chapter has received the Special Appreciation Award for its outstanding contributions to technical education and student engagement.
+    </p>
+
+    <p>
+      The Best Student Awards are also given to recognize exceptional individuals who demonstrate excellence in academics, 
+      leadership, and active participation in the chapter's activities. The ISTE GECK Student Chapter continues to uphold a tradition of excellence, 
+      making significant strides in creating future-ready professionals and leaders in engineering and technology.
+    </p>
+
+
+  </div>
+</div>
 
 
     </div>
