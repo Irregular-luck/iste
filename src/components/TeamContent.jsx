ee2e279be740a5ee1f7e51497b0c143adced5341
Nidal <nidalthegreat69664@gmail.com>
2026-01-04 16:21:58 +0530

    image slider added

--- src/components/TeamContent.jsx
+++ src/components/TeamContent.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
 import './TeamContent.css'
+import Lightbox from "yet-another-react-lightbox";
+import "yet-another-react-lightbox/styles.css";
 
 
 const TeamContent = () => {
+    const [open, setopen] = React.useState(false)
     return (
         <div className='team'>
             <div className='title'>
@@ -13,16 +16,33 @@ const TeamContent = () => {
             </div>
             <div className='image-frames'>
                 <div className='frame'>
-                    <img src="frame1.png" alt="team" className='image-ani' />
+                    <button onClick={()=>setopen(true)}>
+                        <img src="frame1.png" alt="team" className='image-ani' />
+                    </button>
                 </div>
                 <div className='frame'>
-                    <img src="frame2.png" alt="team" className='image-ani' />
+                     <button onClick={()=>setopen(true)}>
+                        <img src="frame2.png" alt="team" className='image-ani' />
+                      </button>  
+                
                 </div>
                 <div className='frame'>
-                    <img src="frame3.png" alt="team" className='image-ani' />
+                     <button onClick={()=>setopen(true)}>
+                        <img src="frame3.png" alt="team" className='image-ani' />
+                        </button>
                 </div>
             </div>
+            <Lightbox
+            open={open}
+            close={() => setopen(false)}
+            slides={[
+          { src: "frame1.png" },
+          { src: "frame2.png" },
+          { src: "frame3.png" },
+        ]}
+            />
         </div>
+        
     )
 }
 
