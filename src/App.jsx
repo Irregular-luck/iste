9c765acbf3c117279267ad650217b57b08ebd43d
adithyan <nadithyan837@gmail.com>
2026-03-05 02:04:28 +0530

    loading animation

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,42 @@
-import React from 'react'
-import Home from './pages/Home'
-import Events from './pages/Events'
-import Team from './pages/Team'
-import Gallery from './pages/Gallery'
-import RouteLayout from './Layout/RouteLayout'
-import { Route,RouterProvider,createBrowserRouter,createRoutesFromElements } from 'react-router-dom'
-import Login from './pages/Login'
-import { Navigate } from 'react-router-dom'
-const App = () => {
-  
-  
-  const router = createBrowserRouter(createRoutesFromElements(
-  
-    <Route path='/' element={ <RouteLayout/> }>
-      <Route index element={ <Home/> }></Route>
-      <Route path='Events' element={ <Events/> }></Route>
-      <Route path='Team' element={ <Team/> }></Route>
-      <Route path='Gallery' element={ <Gallery/> }></Route>
-      <Route path='login' element={<Login/>}></Route>
+import React, { useState, useEffect } from "react";
+import Home from "./pages/Home";
+import Events from "./pages/Events";
+import Team from "./pages/Team";
+import Gallery from "./pages/Gallery";
+import RouteLayout from "./Layout/RouteLayout";
+import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from "react-router-dom";
+import Login from "./pages/Login";
+import Loader from "./components/Loader";
 
-
-
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<RouteLayout />}>
+      <Route index element={<Home />} />
+      <Route path="Events" element={<Events />} />
+      <Route path="Team" element={<Team />} />
+      <Route path="Gallery" element={<Gallery />} />
+      <Route path="login" element={<Login />} />
     </Route>
-    
-    
-  ))
-  
-  return <RouterProvider router={router} />
+  )
+);
 
+const App = () => {
 
-}
-export default App
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 2500);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  if (loading) {
+    return <Loader />;
+  }
+
+  return <RouterProvider router={router} />;
+};
+
+export default App;
---
+++ src/components/Loader.jsx
@@ -0,0 +1,11 @@
+import "./Loader.css";
+
+export default function Loader() {
+  return (
+    <div className="loader-container">
+      <div className="logo-mask">
+        <div className="water-fill"></div>
+      </div>
+    </div>
+  );
+}
